6de7fb722a06cb26fa8c44ec3811f9864434fb39
Austin <austinm9506@gmail.com>
2022-05-13 03:55:35 -0400

    Added middleware and imported API.

--- server.ts
+++ server.ts
@@ -1,6 +1,14 @@
 import express from "express"
 
+const { bugsRouter } = require("./controller/bugsApi")
+
 const app = express() 
+
+app.use(express.json())
+
+app.use(express.urlencoded({extended: true}))
+
+app.use("/", bugsRouter)
 
 const PORT = process.env.PORT || 8080
 
