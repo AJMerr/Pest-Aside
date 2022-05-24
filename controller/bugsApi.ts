c6a5c2e4e530c5480d765fcacd8da6e5f3458702
Austin <austinm9506@gmail.com>
2022-05-24 18:59:59 -0400

    Refactored code to follow Try/Catch syntax. Updated the delete function to return a status.

--- controller/bugsApi.ts
+++ controller/bugsApi.ts
@@ -7,75 +7,76 @@ const prisma = new PrismaClient()
 
 // Get all bugs
 bugsRouter.get("/api", async (_req, res) => {
-    return await prisma.bug.findMany()
-    .then((allBugs: object) => {
+    try {
+        const allBugs = await prisma.bug.findMany()
+        if (!allBugs) {
+            throw new Error("Unable to find bugs")
+        }
         res.json(allBugs)
-    })
-    .catch((err: Error) => {
+    } catch (err) {
         console.error(err)
-    })
+    }
 })
 
 // Get a bug
 bugsRouter.get("/api/:id", async (req, res) => {
-    const { id } = req.params
-    await prisma.bug.findUnique({
-        where: {
-            id: Number(id)
+    try {
+        const { id } = req.params
+        const bug = await prisma.bug.findUnique({
+            where: {
+                id: Number(id)
+            }
+        })
+        if (!bug) {
+            throw new Error("Bug not found")
         }
-    })
-    .then((oneBug: any) => {
-        res.json(oneBug)
-    })
-    .catch((err) => {
+        res.json(bug)
+    } catch (err) {
         console.error(err)
-    })
+    }
 })
 
 // Create a new bug
 bugsRouter.post("/api/create", async (req, res) => {
-    await prisma.bug.create({
-        data: req.body
-    })
-    .then((newBug: object) => {
+    try {
+        const newBug = await prisma.bug.create({
+            data: req.body
+        })
         res.json(newBug)
-    })
-    .catch((err: Error) => {
+    } catch (err) {
         console.error(err)
-    })
+    }
 })
 
 // Edit a bug
 bugsRouter.put("/api/edit/:id", async (req, res) => {
-    const { id } = req.params
-    await prisma.bug.update({
-        where: {
-            id: Number(id)
-        },
-        data: req.body
-    })
-    .then((updatedBug) => {
+    try {
+        const { id } = req.params
+        const updatedBug = await prisma.bug.update({
+            where: {
+                id: Number(id)
+            },
+            data: req.body
+        })
         res.json(updatedBug)
-    })
-    .catch((err) => {
+    } catch (err) {
         console.error(err)
-    })
+    }
 })
 
 // Delete a bug
 bugsRouter.delete("/api/:id", async (req, res) => {
-    const { id } = req.params
-    await prisma.bug.delete({
-        where: {
-            id: Number(id)
-        }
-    })
-    .then(() => {
-        return console.log("200 OKAY")
-    })
-    .catch((err) => {
+    try {
+        const { id } = req.params
+        await prisma.bug.delete({
+            where: {
+                id: Number(id)
+            }
+        })
+        res.sendStatus(200)
+    } catch (err) {
         console.error(err)
-    })
+    }
 })
 
 module.exports = {
