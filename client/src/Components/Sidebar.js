cf3873edc8a8319ac289a71f5226d9b30b760589
Austin <austinm9506@gmail.com>
2022-05-27 19:50:42 -0400

    Added Tailwind for front end UI design.

---
+++ client/src/Components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from "react" 
+
+function Sidebar() {
+    return(
+        <div className="fixed top-0 left-0 h-screen w-16 bg-slate-700 text-yellow-50 flex flex-col items-center">
+            <i>1</i>
+            <i>1</i>
+            <i>1</i>
+            <i>1</i>
+        </div>
+    )
+}
+
+export default Sidebar
